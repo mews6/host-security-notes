1fed5988dd44fbb7a8018aa3a1476a7d5974094a
Jaime Torres <jatber.mrowr@gmail.com>
2025-01-23 19:04:01 -0500

    add example

---
+++ examples/other/c-declarations.c
@@ -0,0 +1,9 @@
+int i; // integer
+int *p; // pointer
+int v[10]; // vector of 10 integers
+int i,j,k; // multiple integers at once
+int * p, i, j ; // pointer and two integers
+int i, *p,v[10]; //int data, pointer and vector
+int **pp; //pointer to a ponter
+int * vp[10]; //vector of integer pointers
+char (*f)(int); // pointer to a function that recieves an integer and returns a char
